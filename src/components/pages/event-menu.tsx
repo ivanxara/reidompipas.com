b0c2029e423f1b781049604e49ffac4c5d172572
Ivan Xará <ivanmxara@gmail.com>
2024-09-14 14:52:55 +0100

    dev: test pdf

--- src/components/pages/event-menu.tsx
+++ src/components/pages/event-menu.tsx
@@ -17,31 +17,11 @@ export default function EventMenu({
 }) {
   const router = useRouter();
   return (
-    <div className="w-full h-full">
-      {/* <Wrapper>
-        <Button variant="capsule" onClick={() => router.back()}>
-          <ChevronLeft className="size-4" />
-          <span>Voltar</span>
-        </Button>
-        <div className="flex flex-col">
-          <Heading2 className="mt-4">{title}</Heading2>
-          <span className="text-xs text-muted-foreground">
-            Se o PDF não estiver disponível, clique no botão para fazer download
-            do menu
-          </span>
-          <Button className="mt-2 w-fit">
-            <a href={src} download>
-              Download PDF
-            </a>
-          </Button>
-        </div>
-      </Wrapper> */}
-      <embed
-        className="absolute left-0 top-0 w-full h-full"
-        src={src}
-        title="PDF Viewer"
-        aria-label="PDF Viewer for the executive menu"
-      />
-    </div>
+    <iframe
+      style={{ position: "absolute", left: 0, top: 0 }}
+      width="100%"
+      height="100%"
+      src="https://reidompipas.com/files/menu-executivo.pdf"
+    />
   );
 }
