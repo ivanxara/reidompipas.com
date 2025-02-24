18b805abd68f760d06ccbdc543cb2a4799244b11
ivanxara <ivanmxara@gmail.com>
2025-02-24 14:13:06 +0000

    fix: carnaval text

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -33,7 +33,7 @@ export const EVENTS = [
   {
     label: "Menu Carnaval",
     subLabel:
-      "🎭 Dia 3 de Março Venha festejar o Carnaval connosco com muita animação, boa comida e DJ ao vivo! 🔥",
+      "🎭 Dia 3 de Março Venha festejar a noite de Carnaval connosco com muita animação, boa comida e DJ ao vivo! 🔥",
     image: ImageMenuCarnaval,
     to: "/eventos/carnaval",
   },
